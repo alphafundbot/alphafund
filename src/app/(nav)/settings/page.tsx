42b76063ce715e0c99b905e92d31721dfb947491
Nehemie Destine <cyberdestine@gmail.com>
2025-08-06 14:37:38 +0000

    fix settings

--- src/app/(nav)/settings/page.tsx
+++ src/app/(nav)/settings/page.tsx
@@ -1,13 +1,11 @@
+
 import { fetchConfig } from '@/app/actions';
 import { FirebaseConfigClient } from '@/components/firebase-config-client';
-import { AppLayout } from '@/components/layout';
 
 export default async function SettingsPage() {
   const config = await fetchConfig();
 
   return (
-    <AppLayout>
-        <FirebaseConfigClient initialConfig={config} />
-    </AppLayout>
+    <FirebaseConfigClient initialConfig={config} />
   );
 }
